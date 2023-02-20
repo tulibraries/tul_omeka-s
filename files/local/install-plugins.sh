909bc1ae180aad435c5ba880f01a8b91eef51b49
Steven Ng <steven.ng@temple.edu>
2023-02-20 14:07:28 -0800

    Update modules for Omeka-S 4.0.0

--- files/local/install-plugins.sh
+++ files/local/install-plugins.sh
@@ -8,15 +8,14 @@ if [ -d "/var/www/html/modules/AltText" ]; then
 fi
 
 # Install Modules
-wget --no-verbose "https://github.com/omeka-s-modules/Mapping/releases/download/v1.4.1/Mapping-1.4.1.zip" && unzip -o Mapping-1.4.1.zip -d /var/www/html/modules/ && rm Mapping-1.4.1.zip 
-wget --no-verbose "https://github.com/Daniel-KM/Omeka-S-module-OaiPmhRepository/releases/download/3.3.5.4/OaiPmhRepository-3.3.5.4.zip" && unzip -o OaiPmhRepository-3.3.5.4.zip -d /var/www/html/modules/ && rm OaiPmhRepository-3.3.5.4.zip 
-wget --no-verbose "https://github.com/Daniel-KM/Omeka-S-module-ImageServer/releases/download/3.6.7.3/ImageServer-3.6.7.3.zip" && unzip -o ImageServer-3.6.7.3.zip -d /var/www/html/modules/ && rm ImageServer-3.6.7.3.zip 
-wget --no-verbose "https://github.com/Daniel-KM/Omeka-S-module-IiifServer/releases/download/3.6.5.3/IiifServer-3.6.5.3.zip" && unzip -o IiifServer-3.6.5.3.zip -d /var/www/html/modules/ && rm IiifServer-3.6.5.3.zip 
-wget --no-verbose "https://github.com/omeka-s-modules/CSVImport/releases/download/v2.2.1/CSVImport-2.2.1.zip" && unzip -o CSVImport-2.2.1.zip -d /var/www/html/modules/ && rm CSVImport-2.2.1.zip 
-wget --no-verbose "https://github.com/Daniel-KM/Omeka-S-module-BulkEdit/releases/download/3.3.12.4/BulkEdit-3.3.12.4.zip" && unzip -o BulkEdit-3.3.12.4.zip -d /var/www/html/modules/ && rm BulkEdit-3.3.12.4.zip 
-wget --no-verbose "https://github.com/omeka-s-modules/CSSEditor/releases/download/v1.3.0/CSSEditor-1.3.0.zip" && unzip -o CSSEditor-1.3.0.zip -d /var/www/html/modules/ && rm CSSEditor-1.3.0.zip 
-wget --no-verbose "https://github.com/omeka-s-modules/CSVImport/releases/download/v2.3.2/CSVImport-2.3.2.zip" && unzip -o CSVImport-2.3.2.zip -d /var/www/html/modules/ && rm CSVImport-2.3.2.zip 
-wget --no-verbose "https://github.com/omeka-s-modules/Omeka2Importer/releases/download/v1.4.0/Omeka2Importer-1.4.0.zip" && unzip -o Omeka2Importer-1.4.0.zip -d /var/www/html/modules/ && rm Omeka2Importer-1.4.0.zip
+wget --no-verbose "https://github.com/omeka-s-modules/Mapping/releases/download/v1.8.0/Mapping-1.8.0.zip" && unzip -o Mapping-1.8.0.zip -d /var/www/html/modules/ && rm Mapping-1.8.0.zip
+wget --no-verbose "https://github.com/Daniel-KM/Omeka-S-module-OaiPmhRepository/releases/download/3.4.6/OaiPmhRepository-3.4.6.zip" && unzip -o OaiPmhRepository-3.4.6.zip -d /var/www/html/modules/ && rm OaiPmhRepository-3.4.6.zip
+wget --no-verbose "https://github.com/Daniel-KM/Omeka-S-module-ImageServer/releases/download/3.6.12.4/ImageServer-3.6.12.4.zip" && unzip -o ImageServer-3.6.12.4.zip -d /var/www/html/modules/ && rm ImageServer-3.6.12.4.zip
+wget --no-verbose "https://github.com/Daniel-KM/Omeka-S-module-IiifServer/releases/download/3.6.9/IiifServer-3.6.9.zip" && unzip -o IiifServer-3.6.9.zip -d /var/www/html/modules/ && rm IiifServer-3.6.9.zip
+wget --no-verbose "https://github.com/omeka-s-modules/CSVImport/releases/download/v2.4.1/CSVImport-2.4.1.zip" && unzip -o CSVImport-2.4.1.zip -d /var/www/html/modules/ && rm CSVImport-2.4.1.zip
+wget --no-verbose "https://github.com/Daniel-KM/Omeka-S-module-BulkEdit/releases/download/3.4.18/BulkEdit-3.4.18.zip" && unzip -o BulkEdit-3.4.18.zip -d /var/www/html/modules/ && rm BulkEdit-3.4.18.zip
+wget --no-verbose "https://github.com/omeka-s-modules/CSSEditor/releases/download/v1.3.1/CSSEditor-1.3.1.zip" && unzip -o CSSEditor-1.3.1.zip -d /var/www/html/modules/ && rm CSSEditor-1.3.1.zip
+wget --no-verbose "https://github.com/omeka-s-modules/Omeka2Importer/releases/download/v1.5.1/Omeka2Importer-1.5.1.zip" && unzip -o Omeka2Importer-1.5.1.zip -d /var/www/html/modules/ && rm Omeka2Importer-1.5.1.zip
 
 # Install Themes
 wget --no-verbose "https://github.com/omeka-s-themes/default/releases/download/v1.7.1/default-1.7.1.zip" && unzip -o default-1.7.1.zip -d /var/www/html/themes/ && rm default-1.7.1.zip 
