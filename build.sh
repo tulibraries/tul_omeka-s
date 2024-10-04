28449e51230fcaaa76451e8a07ab36818785b78c
David Kinzer <dtkinzer@gmail.com>
2024-10-04 14:14:46 -0400

    Fix omeka not building.

--- build.sh
+++ build.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env sh
+set -e
 
+echo "BUILDING OMEKA"
 
-if [ $OMEKA_VERSION = "" ];
-then
-  OMEKA_VERSION=4.0.4
-fi
+cd ./omeka-s
+git submodule update --recursive --force
 
-echo "BUILDING OMEKA (VERSION $OMEKA_VERSION)"
-wget --no-verbose "https://github.com/omeka/omeka-s/releases/download/v${OMEKA_VERSION}/omeka-s-${OMEKA_VERSION}.zip" -O latest_omeka_s.zip
+echo "APPLY OMEKA SECURITY PATCHES"
+npm audit fix --omit dev
+npm install
+npx gulp init
 
-unzip -q latest_omeka_s.zip -d /var/www/
-rm latest_omeka_s.zip
+cd -
+
 rm -rf /var/www/html/
-mv /var/www/omeka-s/ /var/www/html/
+mv ./omeka-s/ /var/www/html/
 
 cp -f -p files/omeka/application/src/Form/LoginForm.php /var/www/html/application/src/Form/
 cp -f -p files/omeka/application/config/application.config.php /var/www/html/application/config/
@@ -32,18 +34,25 @@ cp .htaccess /var/www/html/.htaccess
 cp database.ini /var/www/html/config/
 cp local.config.php /var/www/html/config/
 cp -pr files/apache2/httpd.conf /etc/apache2
-cp -pr files/local/install-plugins.sh /var/www/html
 
 echo "ServerName localhost" >> /etc/apache2/httpd.conf
 
+echo "INSTALLING OMEKA MODULES"
+for dir in $(./modules); do
+  mv ./modules/$dir /var/www/html/modules/
+done
+
+
+echo "INSTALLING OMEKA THEMES"
+for dir in $(./themes); do
+  mv ./themes/$dir /var/www/html/themes/
+done
+
+echo "CLEANING UP"
 chown -R nobody:nobody /var/www/html/
 chown -R nobody:nobody /run/apache2/
 chmod 600 /var/www/html/config/database.ini
 chmod 600 /var/www/html/config/local.config.php
 chmod 600 /var/www/html/.htaccess
-chmod 700 /var/www/html/install-plugins.sh
 rm -rf /build
-
-echo "INSTALLING OMEKA PLUGINGS"
-cd /var/www/html
-su -s /bin/sh nobody -c '/var/www/html/install-plugins.sh'
+apk del build-dependencies
