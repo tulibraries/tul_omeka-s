7632c33f0c5d10d3a7cfab42434ac24b529cf634
David Kinzer <dtkinzer@gmail.com>
2024-10-04 16:57:12 -0400

    Fix modules/themes not installing.

--- build.sh
+++ build.sh
@@ -7,12 +7,13 @@ git submodule init
 git submodule update --recursive --force
 
 cd ./omeka-s
-
 echo "APPLY OMEKA SECURITY PATCHES"
 npm audit fix --omit dev
 npm install
 npx gulp init
 
+echo "UNINSTALL ALL OMEKA DEV DEPENDENCIES"
+npm uninstall $(jq -r '.devDependencies | keys | join(" ")' < package.json) --save-dev
 cd -
 
 rm -rf /var/www/html/
@@ -40,14 +41,14 @@ cp -pr files/apache2/httpd.conf /etc/apache2
 echo "ServerName localhost" >> /etc/apache2/httpd.conf
 
 echo "INSTALLING OMEKA MODULES"
-for dir in $(./modules); do
-  mv ./modules/$dir /var/www/html/modules/
+for dir in $(ls ./modules); do
+  mv ./modules/$dir /var/www/html/modules/$dir
 done
 
 
 echo "INSTALLING OMEKA THEMES"
-for dir in $(./themes); do
-  mv ./themes/$dir /var/www/html/themes/
+for dir in $(ls ./themes); do
+  mv ./themes/$dir /var/www/html/themes/$dir
 done
 
 echo "CLEANING UP"
@@ -56,5 +57,6 @@ chown -R nobody:nobody /run/apache2/
 chmod 600 /var/www/html/config/database.ini
 chmod 600 /var/www/html/config/local.config.php
 chmod 600 /var/www/html/.htaccess
+
 rm -rf /build
 apk del build-dependencies
